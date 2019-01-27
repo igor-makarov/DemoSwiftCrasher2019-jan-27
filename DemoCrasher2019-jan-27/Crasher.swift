c04a567e404c485d400fa7267c8376983efeed05
Igor Makarov <igormaka@gmail.com>
2019-01-27 10:48:48 +0200

    reduce amount of code in crasher

--- DemoCrasher2019-jan-27/Crasher.swift
+++ DemoCrasher2019-jan-27/Crasher.swift
@@ -18,34 +18,12 @@ extension NSObject: IdentifiableType {
 
 typealias SectionModelImpl = AnimatableSectionModel<NSObject, NSObject>
 
-@objc(RxTableViewSectionedDataSource)
-public final class RxTableViewSectionedReloadDataSourceObjC: NSObject {
-    @objc
-    public final var tableView: UITableView? {
-        didSet {
-            rebindDataSource()
-        }
-    }
-    
-    @objc
-    public final var cellForRow: CellForRowBlock? {
-        didSet {
-            rebindDataSource()
-        }
-    }
-    
-    @objc
-    public final var canEditRow: CanEditRowBlock? {
-        didSet {
-            rebindDataSource()
-        }
-    }
-    
-    weak var dataSource: TableViewSectionedDataSource<SectionModelImpl>?
-    
+public class RxTableViewSectionedReloadDataSourceObjC: NSObject {
+    public var tableView: UITableView?
+    public var cellForRow: CellForRowBlock?
+    public var canEditRow: CanEditRowBlock?
     
     func rebindDataSource() {
-        let disposeBag = DisposeBag()
         guard let tableView = tableView else { return }
         guard let cellforRow = cellForRow else { return }
         let canEditRowBlock: ((TableViewSectionedDataSource<SectionModelImpl>, IndexPath) -> Bool)
@@ -54,13 +32,10 @@ public final class RxTableViewSectionedReloadDataSourceObjC: NSObject {
         let observableSignal = Observable.just([SectionModelImpl]())
         
         let dataSource = RxTableViewSectionedAnimatedDataSource<SectionModelImpl>(configureCell: { cellforRow($1, $2, $3) })
-        self.dataSource = dataSource
         
         dataSource.canEditRowAtIndexPath = canEditRowBlock
         
-        observableSignal
-            .bind(to: tableView.rx.items(dataSource: dataSource))
-            .disposed(by: disposeBag)
+        _ = observableSignal.bind(to: tableView.rx.items(dataSource: dataSource))
     }
     
 }
